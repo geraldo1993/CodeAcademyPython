8db081bbefb2c0b7bce6a0f50a79f3ac85eb6a5e
Geraldo Braho <gbraho@na.edu>
2016-09-18 17:25:38 -0500

    Create Compare... Closelier!.py

---
+++ Conditionals & Control Flow/Compare... Closelier!.py
@@ -0,0 +1,33 @@
+'''Excellent! It looks like you're comfortable with basic expressions and comparators.
+
+But what about extreme expressions and comparators?
+
+Instructions
+Let's run through the comparators again with more complex expressions. Set each variable to True or False depending on what you think the result will be.
+
+Set bool_one to the result of (20 - 10) > 15
+Set bool_two to the result of (10 + 17) == 3**16
+Set bool_three to the result of 1**2 <= -1
+Set bool_four to the result of 40 * 4 >= -4
+Set bool_five to the result of 100 != 10**2'''
+
+
+
+
+# Assign True or False as appropriate on the lines below!
+
+# (20 - 10) > 15
+bool_one = False    # We did this one for you!
+
+# (10 + 17) == 3**16
+# Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
+bool_two = False
+
+# 1**2 <= -1
+bool_three = False
+
+# 40 * 4 >= -4
+bool_four = True
+
+# 100 != 10**2
+bool_five = False
