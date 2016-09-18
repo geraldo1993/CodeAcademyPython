7ebc2876838cea045ee9d27a913fcc74b4366548
Geraldo Braho <gbraho@na.edu>
2016-09-18 16:25:53 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#Creating web apps, games, and search engines all involve storing and working with different types of data. They do so using variables. A variable stores a piece of data, and gives it a specific name.
+
+#For example:
+
+#spam = 5
+#The variable spam now stores the number 5.
+
+#Instructions
+#Set the variable my_variable equal to the value 10.
+#Click the Save & Submit button to run your code.
+
+
+
+# Write your code below!
+my_variable=10
