d1826aa19fd6c612d2e361aa02859ab567a403ff
Geraldo Braho <gbraho@na.edu>
2016-09-18 16:28:17 -0500

    Update Booleans.py

--- Booleans.py
+++ Booleans.py
@@ -8,9 +8,15 @@ You can use variables to store booleans like this:
 a = True
 b = False
 Instructions
-Set the following variables to the corresponding values:'''
+Set the following variables to the corresponding values:
 
 
 my_int to the value 7
 my_float to the value 1.23
 my_bool to the value True
+'''
+# Set the variables to the values listed in the instructions!
+my_int=7
+my_float=1.23
+my_bool=True
+
