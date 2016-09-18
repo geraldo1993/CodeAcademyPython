21130524f3d7822d3c192a78107e9e55682d2cb0
Geraldo Braho <gbraho@na.edu>
2016-09-18 17:05:03 -0500

    Create And Now, For Something Completely Familiar.py

---
+++ Strings & Console Output/And Now, For Something Completely Familiar.py
@@ -0,0 +1,36 @@
+'''Great job! You've learned a lot in this unit, including:
+
+Three ways to create strings
+
+'Alpha'
+"Bravo"
+str(3)
+String methods
+
+len("Charlie")
+"Delta".upper()
+"Echo".lower()
+Printing a string
+
+print "Foxtrot"
+Advanced printing techniques
+
+g = "Golf"
+h = "Hotel"
+print "%s, %s" % (g, h)
+Instructions
+Let's wrap it all up!
+
+On line 3, create the variable my_string and set it to any string you'd like.
+On line 4, print the length of my_string.
+On line 5, print the .upper() case version of my_string.'''
+
+
+
+
+
+# Write your code below, starting on line 3!
+
+my_string="geraldo"
+print len(my_string)
+print my_string.upper()
