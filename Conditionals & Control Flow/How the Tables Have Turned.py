3e261db948bafb24818cdb966960f98c1c2c3b41
Geraldo Braho <gbraho@na.edu>
2016-09-18 17:28:49 -0500

    Create How the Tables Have Turned.py

---
+++ Conditionals & Control Flow/How the Tables Have Turned.py
@@ -0,0 +1,33 @@
+'''Comparisons result in either True or False, which are booleans as we learned before in this exercise.
+
+# Make me true!
+bool_one = 3 < 5
+Let's switch it up: we'll give the boolean, and you'll write the expression, just like the example above.
+
+Instructions
+For each boolean value in the editor, write an expression that evaluates to that value.
+
+Remember, comparators are: ==, !=, >, >=, <, and <=.
+
+Use at least three different ones!
+
+Don't just use True and False! That's cheating!'''
+
+
+
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 3 < 5  # We already did this one for you!
+
+# Make me false!
+bool_two = 5 < 3
+ 
+# Make me true!
+bool_three = 4<=(3+1)
+
+# Make me false!
+bool_four = 4<=(2+1)
+
+# Make me true!
+bool_five = 10 ==(5*2)
