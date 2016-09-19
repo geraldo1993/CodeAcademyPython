d4d1b6ff8a9638ecdb6b036d07bf6a419c3b7145
Geraldo Braho <gbraho@na.edu>
2016-09-19 04:47:11 -0500

    Create Mix 'n' Match.py

---
+++ Conditionals & Control Flow/Mix 'n' Match.py
@@ -0,0 +1,26 @@
+'''Great work! We're almost done with boolean operators.
+
+# Make me false
+bool_one = (2 <= 2) and "Alpha" == "Bravo"
+Instructions
+This time we'll give the expected result, and you'll use some combination of boolean operators to achieve that result.
+
+Remember, the boolean operators are and, or, and not. Use each one at least once!'''
+
+
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = 3==3 and 9>8     
+
+# Make me false!
+bool_three = not True and False
+
+# Make me true!
+bool_four = not False and True 
+
+# Make me true!
+bool_five = 0 or 1
